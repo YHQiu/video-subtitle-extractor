2bd98f57da8376542b06c65cf6f745dc8fac323b
qiuhongyu <qiuhongyu@feimanai.com>
2024-03-18 11:34:12 +0800

    feat:add docker surport and api surpot

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def extractor():
             '--x-max', str(subtitle_area[3]),
             '--gpu_mem', str(2048),
             '--max_batch_size', str(64),
-            '--cls_batch_num', str(64),
+            '--cls_batch_num', str(4),
         ]
 
         try:
