b575d7a5417585bb8d732cb768a11b69f12ad898
qiuhongyu <qiuhongyu@feimanai.com>
2024-03-20 23:33:31 +0800

    fix

--- backend/config.py
+++ backend/config.py
@@ -212,7 +212,7 @@ GENERATE_TXT = True
 REC_BATCH_NUM = 1
 # DB算法每个batch识别多少张，默认为10
 # MAX_BATCH_SIZE = 10
-MAX_BATCH_SIZE = 25
+MAX_BATCH_SIZE = 50
 
 # 默认字幕出现区域为下方
 DEFAULT_SUBTITLE_AREA = SubtitleArea.UNKNOWN
